dd762086ae392e36b551f61bd547a681ae3baa0a
Roberto Fonseca <roberto@rhfactor.com.br>
2021-08-06 18:05:16 -0300

    Configurando o projeto com Gradle

---
+++ src/main/java/br/com/rhfactor/samplecrudapi/SampleCrudApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.rhfactor.samplecrudapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleCrudApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleCrudApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/rhfactor/samplecrudapi/SampleCrudApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rhfactor.samplecrudapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleCrudApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
